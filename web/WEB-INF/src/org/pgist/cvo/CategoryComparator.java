e2889a768f122a922feea2ef533fa5329e0e854c
kenny <>
2006-01-26 14:53:34 +0000

    initial.

---
+++ web/WEB-INF/src/org/pgist/cvo/CategoryComparator.java
@@ -0,0 +1,21 @@
+package org.pgist.cvo;
+
+import java.util.Comparator;
+
+
+public class CategoryComparator implements Comparator {
+    
+    
+    public int compare(Object obj1, Object obj2) throws ClassCastException {
+        if (obj1==obj2) return 0;
+        
+        Category cat1 = (Category) obj1;
+        Category cat2 = (Category) obj2;
+        
+        if (cat1.getName()==null) return -1;
+        
+        return cat1.getName().compareTo(cat2.getName());
+    }//compare()
+    
+
+}
