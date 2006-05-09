76c13d0068c8b5cbc7b175baccb8babf14ffff4a
wujie <>
2006-05-09 21:30:00 +0000

    *** empty log message ***

---
+++ web/WEB-INF/src/org/pgist/cvo/StopWord.java
@@ -0,0 +1,39 @@
+package org.pgist.cvo;
+
+/**
+ * @author Jie
+ * @version 1.0
+ * @hibernate.class table="pgist_cvo_tags" lazy="true"
+ */
+
+public class StopWord {
+    protected Long id;
+
+    protected String name;
+
+
+    /**
+     * @return
+     * @hibernate.id generator-class="native"
+     */
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return
+     * @hibernate.property not-null="true"
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+           this.name = name;
+       }
+
+}
