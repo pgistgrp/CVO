e31e7e5dcaec4b995f95ed20370e973baa61d1cc
guirong <>
2006-05-31 02:23:48 +0000

    *** empty log message ***

---
+++ stopwords.sql
@@ -0,0 +1,549 @@
+insert into pgist_cvo_stopwords (name) values ("a
+")
+insert into pgist_cvo_stopwords (name) values ("able
+")
+insert into pgist_cvo_stopwords (name) values ("about
+")
+insert into pgist_cvo_stopwords (name) values ("absolutely
+")
+insert into pgist_cvo_stopwords (name) values ("accordingly
+")
+insert into pgist_cvo_stopwords (name) values ("actually
+")
+insert into pgist_cvo_stopwords (name) values ("affected
+")
+insert into pgist_cvo_stopwords (name) values ("affecting
+")
+insert into pgist_cvo_stopwords (name) values ("after
+")
+insert into pgist_cvo_stopwords (name) values ("again
+")
+insert into pgist_cvo_stopwords (name) values ("against
+")
+insert into pgist_cvo_stopwords (name) values ("all
+")
+insert into pgist_cvo_stopwords (name) values ("almost
+")
+insert into pgist_cvo_stopwords (name) values ("already
+")
+insert into pgist_cvo_stopwords (name) values ("also
+")
+insert into pgist_cvo_stopwords (name) values ("although
+")
+insert into pgist_cvo_stopwords (name) values ("always
+")
+insert into pgist_cvo_stopwords (name) values ("am
+")
+insert into pgist_cvo_stopwords (name) values ("ame
+")
+insert into pgist_cvo_stopwords (name) values ("among
+")
+insert into pgist_cvo_stopwords (name) values ("an
+")
+insert into pgist_cvo_stopwords (name) values ("and
+")
+insert into pgist_cvo_stopwords (name) values ("another
+")
+insert into pgist_cvo_stopwords (name) values ("any
+")
+insert into pgist_cvo_stopwords (name) values ("anyone
+")
+insert into pgist_cvo_stopwords (name) values ("apparently
+")
+insert into pgist_cvo_stopwords (name) values ("are
+")
+insert into pgist_cvo_stopwords (name) values ("arise
+")
+insert into pgist_cvo_stopwords (name) values ("as
+")
+insert into pgist_cvo_stopwords (name) values ("asidee
+")
+insert into pgist_cvo_stopwords (name) values ("at
+")
+insert into pgist_cvo_stopwords (name) values ("away
+")
+insert into pgist_cvo_stopwords (name) values ("back
+")
+insert into pgist_cvo_stopwords (name) values ("basically
+")
+insert into pgist_cvo_stopwords (name) values ("be
+")
+insert into pgist_cvo_stopwords (name) values ("became
+")
+insert into pgist_cvo_stopwords (name) values ("because
+")
+insert into pgist_cvo_stopwords (name) values ("become
+")
+insert into pgist_cvo_stopwords (name) values ("becomes
+")
+insert into pgist_cvo_stopwords (name) values ("been
+")
+insert into pgist_cvo_stopwords (name) values ("before
+")
+insert into pgist_cvo_stopwords (name) values ("being
+")
+insert into pgist_cvo_stopwords (name) values ("believe
+")
+insert into pgist_cvo_stopwords (name) values ("better
+")
+insert into pgist_cvo_stopwords (name) values ("between
+")
+insert into pgist_cvo_stopwords (name) values ("both
+")
+insert into pgist_cvo_stopwords (name) values ("briefly
+")
+insert into pgist_cvo_stopwords (name) values ("but
+")
+insert into pgist_cvo_stopwords (name) values ("by
+")
+insert into pgist_cvo_stopwords (name) values ("can
+")
+insert into pgist_cvo_stopwords (name) values ("cannot
+")
+insert into pgist_cvo_stopwords (name) values ("certain
+")
+insert into pgist_cvo_stopwords (name) values ("certainly
+")
+insert into pgist_cvo_stopwords (name) values ("come
+")
+insert into pgist_cvo_stopwords (name) values ("could
+")
+insert into pgist_cvo_stopwords (name) values ("did
+")
+insert into pgist_cvo_stopwords (name) values ("do
+")
+insert into pgist_cvo_stopwords (name) values ("does
+")
+insert into pgist_cvo_stopwords (name) values ("doing
+")
+insert into pgist_cvo_stopwords (name) values ("don
+")
+insert into pgist_cvo_stopwords (name) values ("done
+")
+insert into pgist_cvo_stopwords (name) values ("during
+")
+insert into pgist_cvo_stopwords (name) values ("each
+")
+insert into pgist_cvo_stopwords (name) values ("either
+")
+insert into pgist_cvo_stopwords (name) values ("else
+")
+insert into pgist_cvo_stopwords (name) values ("enough
+")
+insert into pgist_cvo_stopwords (name) values ("etc
+")
+insert into pgist_cvo_stopwords (name) values ("ever
+")
+insert into pgist_cvo_stopwords (name) values ("every
+")
+insert into pgist_cvo_stopwords (name) values ("fact
+")
+insert into pgist_cvo_stopwords (name) values ("following
+")
+insert into pgist_cvo_stopwords (name) values ("for
+")
+insert into pgist_cvo_stopwords (name) values ("found
+")
+insert into pgist_cvo_stopwords (name) values ("from
+")
+insert into pgist_cvo_stopwords (name) values ("further
+")
+insert into pgist_cvo_stopwords (name) values ("gave
+")
+insert into pgist_cvo_stopwords (name) values ("get
+")
+insert into pgist_cvo_stopwords (name) values ("gets
+")
+insert into pgist_cvo_stopwords (name) values ("give
+")
+insert into pgist_cvo_stopwords (name) values ("given
+")
+insert into pgist_cvo_stopwords (name) values ("giving
+")
+insert into pgist_cvo_stopwords (name) values ("go
+")
+insert into pgist_cvo_stopwords (name) values ("going
+")
+insert into pgist_cvo_stopwords (name) values ("gone
+")
+insert into pgist_cvo_stopwords (name) values ("got
+")
+insert into pgist_cvo_stopwords (name) values ("had
+")
+insert into pgist_cvo_stopwords (name) values ("hardly
+")
+insert into pgist_cvo_stopwords (name) values ("has
+")
+insert into pgist_cvo_stopwords (name) values ("have
+")
+insert into pgist_cvo_stopwords (name) values ("having
+")
+insert into pgist_cvo_stopwords (name) values ("here
+")
+insert into pgist_cvo_stopwords (name) values ("how
+")
+insert into pgist_cvo_stopwords (name) values ("however
+")
+insert into pgist_cvo_stopwords (name) values ("i
+")
+insert into pgist_cvo_stopwords (name) values ("if
+")
+insert into pgist_cvo_stopwords (name) values ("im
+")
+insert into pgist_cvo_stopwords (name) values ("in
+")
+insert into pgist_cvo_stopwords (name) values ("into
+")
+insert into pgist_cvo_stopwords (name) values ("is
+")
+insert into pgist_cvo_stopwords (name) values ("it
+")
+insert into pgist_cvo_stopwords (name) values ("itself
+")
+insert into pgist_cvo_stopwords (name) values ("just
+")
+insert into pgist_cvo_stopwords (name) values ("keep
+")
+insert into pgist_cvo_stopwords (name) values ("kept
+")
+insert into pgist_cvo_stopwords (name) values ("knew
+")
+insert into pgist_cvo_stopwords (name) values ("know
+")
+insert into pgist_cvo_stopwords (name) values ("knowledge
+")
+insert into pgist_cvo_stopwords (name) values ("largely
+")
+insert into pgist_cvo_stopwords (name) values ("like
+")
+insert into pgist_cvo_stopwords (name) values ("made
+")
+insert into pgist_cvo_stopwords (name) values ("mainly
+")
+insert into pgist_cvo_stopwords (name) values ("make
+")
+insert into pgist_cvo_stopwords (name) values ("many
+")
+insert into pgist_cvo_stopwords (name) values ("me
+")
+insert into pgist_cvo_stopwords (name) values ("mean
+")
+insert into pgist_cvo_stopwords (name) values ("might
+")
+insert into pgist_cvo_stopwords (name) values ("more
+")
+insert into pgist_cvo_stopwords (name) values ("most
+")
+insert into pgist_cvo_stopwords (name) values ("mostly
+")
+insert into pgist_cvo_stopwords (name) values ("much
+")
+insert into pgist_cvo_stopwords (name) values ("must
+")
+insert into pgist_cvo_stopwords (name) values ("my
+")
+insert into pgist_cvo_stopwords (name) values ("nearly
+")
+insert into pgist_cvo_stopwords (name) values ("necessarily
+")
+insert into pgist_cvo_stopwords (name) values ("neither
+")
+insert into pgist_cvo_stopwords (name) values ("next
+")
+insert into pgist_cvo_stopwords (name) values ("no
+")
+insert into pgist_cvo_stopwords (name) values ("none
+")
+insert into pgist_cvo_stopwords (name) values ("nor
+")
+insert into pgist_cvo_stopwords (name) values ("normally
+")
+insert into pgist_cvo_stopwords (name) values ("not
+")
+insert into pgist_cvo_stopwords (name) values ("noted
+")
+insert into pgist_cvo_stopwords (name) values ("now
+")
+insert into pgist_cvo_stopwords (name) values ("obtain
+")
+insert into pgist_cvo_stopwords (name) values ("obtained
+")
+insert into pgist_cvo_stopwords (name) values ("of
+")
+insert into pgist_cvo_stopwords (name) values ("off
+")
+insert into pgist_cvo_stopwords (name) values ("often
+")
+insert into pgist_cvo_stopwords (name) values ("ok
+")
+insert into pgist_cvo_stopwords (name) values ("on
+")
+insert into pgist_cvo_stopwords (name) values ("one
+")
+insert into pgist_cvo_stopwords (name) values ("only
+")
+insert into pgist_cvo_stopwords (name) values ("or
+")
+insert into pgist_cvo_stopwords (name) values ("other
+")
+insert into pgist_cvo_stopwords (name) values ("our
+")
+insert into pgist_cvo_stopwords (name) values ("out
+")
+insert into pgist_cvo_stopwords (name) values ("over
+")
+insert into pgist_cvo_stopwords (name) values ("owing
+")
+insert into pgist_cvo_stopwords (name) values ("particularly
+")
+insert into pgist_cvo_stopwords (name) values ("past
+")
+insert into pgist_cvo_stopwords (name) values ("people
+")
+insert into pgist_cvo_stopwords (name) values ("perhaps
+")
+insert into pgist_cvo_stopwords (name) values ("please
+")
+insert into pgist_cvo_stopwords (name) values ("poorly
+")
+insert into pgist_cvo_stopwords (name) values ("possible
+")
+insert into pgist_cvo_stopwords (name) values ("possibly
+")
+insert into pgist_cvo_stopwords (name) values ("potentially
+")
+insert into pgist_cvo_stopwords (name) values ("predomiantly
+")
+insert into pgist_cvo_stopwords (name) values ("present
+")
+insert into pgist_cvo_stopwords (name) values ("previously
+")
+insert into pgist_cvo_stopwords (name) values ("primarily
+")
+insert into pgist_cvo_stopwords (name) values ("probably
+")
+insert into pgist_cvo_stopwords (name) values ("prompt
+")
+insert into pgist_cvo_stopwords (name) values ("put
+")
+insert into pgist_cvo_stopwords (name) values ("quickly
+")
+insert into pgist_cvo_stopwords (name) values ("quite
+")
+insert into pgist_cvo_stopwords (name) values ("r
+")
+insert into pgist_cvo_stopwords (name) values ("rather
+")
+insert into pgist_cvo_stopwords (name) values ("readily
+")
+insert into pgist_cvo_stopwords (name) values ("really
+")
+insert into pgist_cvo_stopwords (name) values ("recently
+")
+insert into pgist_cvo_stopwords (name) values ("regarding
+")
+insert into pgist_cvo_stopwords (name) values ("regardless
+")
+insert into pgist_cvo_stopwords (name) values ("relatively
+")
+insert into pgist_cvo_stopwords (name) values ("respectively
+")
+insert into pgist_cvo_stopwords (name) values ("resulted
+")
+insert into pgist_cvo_stopwords (name) values ("resulting
+")
+insert into pgist_cvo_stopwords (name) values ("results
+")
+insert into pgist_cvo_stopwords (name) values ("said
+")
+insert into pgist_cvo_stopwords (name) values ("same
+")
+insert into pgist_cvo_stopwords (name) values ("seem
+")
+insert into pgist_cvo_stopwords (name) values ("seen
+")
+insert into pgist_cvo_stopwords (name) values ("several
+")
+insert into pgist_cvo_stopwords (name) values ("shall
+")
+insert into pgist_cvo_stopwords (name) values ("she
+")
+insert into pgist_cvo_stopwords (name) values ("should
+")
+insert into pgist_cvo_stopwords (name) values ("show
+")
+insert into pgist_cvo_stopwords (name) values ("showed
+")
+insert into pgist_cvo_stopwords (name) values ("shown
+")
+insert into pgist_cvo_stopwords (name) values ("shows
+")
+insert into pgist_cvo_stopwords (name) values ("significantly
+")
+insert into pgist_cvo_stopwords (name) values ("similar
+")
+insert into pgist_cvo_stopwords (name) values ("similarly
+")
+insert into pgist_cvo_stopwords (name) values ("since
+")
+insert into pgist_cvo_stopwords (name) values ("slightly
+")
+insert into pgist_cvo_stopwords (name) values ("so
+")
+insert into pgist_cvo_stopwords (name) values ("some
+")
+insert into pgist_cvo_stopwords (name) values ("something
+")
+insert into pgist_cvo_stopwords (name) values ("sometime
+")
+insert into pgist_cvo_stopwords (name) values ("somewhat
+")
+insert into pgist_cvo_stopwords (name) values ("soon
+")
+insert into pgist_cvo_stopwords (name) values ("specifically
+")
+insert into pgist_cvo_stopwords (name) values ("state
+")
+insert into pgist_cvo_stopwords (name) values ("states
+")
+insert into pgist_cvo_stopwords (name) values ("stop
+")
+insert into pgist_cvo_stopwords (name) values ("strongly
+")
+insert into pgist_cvo_stopwords (name) values ("substantially
+")
+insert into pgist_cvo_stopwords (name) values ("successfully
+")
+insert into pgist_cvo_stopwords (name) values ("such
+")
+insert into pgist_cvo_stopwords (name) values ("sufficiently
+")
+insert into pgist_cvo_stopwords (name) values ("t
+")
+insert into pgist_cvo_stopwords (name) values ("take
+")
+insert into pgist_cvo_stopwords (name) values ("than
+")
+insert into pgist_cvo_stopwords (name) values ("that
+")
+insert into pgist_cvo_stopwords (name) values ("the
+")
+insert into pgist_cvo_stopwords (name) values ("their
+")
+insert into pgist_cvo_stopwords (name) values ("theirs
+")
+insert into pgist_cvo_stopwords (name) values ("them
+")
+insert into pgist_cvo_stopwords (name) values ("then
+")
+insert into pgist_cvo_stopwords (name) values ("there
+")
+insert into pgist_cvo_stopwords (name) values ("therefore
+")
+insert into pgist_cvo_stopwords (name) values ("these
+")
+insert into pgist_cvo_stopwords (name) values ("they
+")
+insert into pgist_cvo_stopwords (name) values ("thing
+")
+insert into pgist_cvo_stopwords (name) values ("things
+")
+insert into pgist_cvo_stopwords (name) values ("think
+")
+insert into pgist_cvo_stopwords (name) values ("this
+")
+insert into pgist_cvo_stopwords (name) values ("those
+")
+insert into pgist_cvo_stopwords (name) values ("though
+")
+insert into pgist_cvo_stopwords (name) values ("through
+")
+insert into pgist_cvo_stopwords (name) values ("throughout
+")
+insert into pgist_cvo_stopwords (name) values ("to
+")
+insert into pgist_cvo_stopwords (name) values ("too
+")
+insert into pgist_cvo_stopwords (name) values ("toward
+")
+insert into pgist_cvo_stopwords (name) values ("transportation
+")
+insert into pgist_cvo_stopwords (name) values ("two
+")
+insert into pgist_cvo_stopwords (name) values ("um
+")
+insert into pgist_cvo_stopwords (name) values ("under
+")
+insert into pgist_cvo_stopwords (name) values ("unless
+")
+insert into pgist_cvo_stopwords (name) values ("until
+")
+insert into pgist_cvo_stopwords (name) values ("upon
+")
+insert into pgist_cvo_stopwords (name) values ("use
+")
+insert into pgist_cvo_stopwords (name) values ("used
+")
+insert into pgist_cvo_stopwords (name) values ("usefullness
+")
+insert into pgist_cvo_stopwords (name) values ("usefully
+")
+insert into pgist_cvo_stopwords (name) values ("using
+")
+insert into pgist_cvo_stopwords (name) values ("usually
+")
+insert into pgist_cvo_stopwords (name) values ("various
+")
+insert into pgist_cvo_stopwords (name) values ("very
+")
+insert into pgist_cvo_stopwords (name) values ("was
+")
+insert into pgist_cvo_stopwords (name) values ("way
+")
+insert into pgist_cvo_stopwords (name) values ("ways
+")
+insert into pgist_cvo_stopwords (name) values ("we
+")
+insert into pgist_cvo_stopwords (name) values ("well
+")
+insert into pgist_cvo_stopwords (name) values ("went
+")
+insert into pgist_cvo_stopwords (name) values ("were
+")
+insert into pgist_cvo_stopwords (name) values ("what
+")
+insert into pgist_cvo_stopwords (name) values ("when
+")
+insert into pgist_cvo_stopwords (name) values ("where
+")
+insert into pgist_cvo_stopwords (name) values ("whether
+")
+insert into pgist_cvo_stopwords (name) values ("which
+")
+insert into pgist_cvo_stopwords (name) values ("while
+")
+insert into pgist_cvo_stopwords (name) values ("who
+")
+insert into pgist_cvo_stopwords (name) values ("whose
+")
+insert into pgist_cvo_stopwords (name) values ("why
+")
+insert into pgist_cvo_stopwords (name) values ("widely
+")
+insert into pgist_cvo_stopwords (name) values ("will
+")
+insert into pgist_cvo_stopwords (name) values ("with
+")
+insert into pgist_cvo_stopwords (name) values ("within
+")
+insert into pgist_cvo_stopwords (name) values ("without
+")
+insert into pgist_cvo_stopwords (name) values ("would
+")
+insert into pgist_cvo_stopwords (name) values ("ya
+")
+insert into pgist_cvo_stopwords (name) values ("yet
+")
+insert into pgist_cvo_stopwords (name) values ("you
+")
+insert into pgist_cvo_stopwords (name) values ("ha")
