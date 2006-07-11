640cc274c6d5977170ded625fdfd12114fc100c0
kenny <>
2006-07-11 00:52:32 +0000

    *** empty log message ***

--- web/WEB-INF/src/org/pgist/glossary/GlossaryPublicAction.java
+++ web/WEB-INF/src/org/pgist/glossary/GlossaryPublicAction.java
@@ -1,6 +1,8 @@
 package org.pgist.glossary;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
@@ -40,10 +42,20 @@ public class GlossaryPublicAction extends Action {
         String direction = "asc";
         Collection terms = glossaryService.getTerms(filter, sort, direction, new int[] {Term.STATUS_OFFICIAL});
         
+        char ch = 0;
+        List initials = new ArrayList(27);
+        for (Term term : (Collection<Term>) terms) {
+            if (ch!=term.getInitial()) {
+                ch = term.getInitial();
+                initials.add(new Character(ch));
+            }
+        }
+        
         request.setAttribute("filter", filter);
         request.setAttribute("sort", sort);
         request.setAttribute("direction", direction);
         request.setAttribute("terms", terms);
+        request.setAttribute("initials", initials);
         
         return mapping.findForward("list");
     }//execute()
--- web/WEB-INF/src/org/pgist/glossary/GlossaryPublicAgent.java
+++ web/WEB-INF/src/org/pgist/glossary/GlossaryPublicAgent.java
@@ -1,7 +1,9 @@
 package org.pgist.glossary;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -78,10 +80,20 @@ public class GlossaryPublicAgent {
             
             Collection terms = glossaryService.getTerms(filter, sort, direction, new int[] {Term.STATUS_OFFICIAL});
             
+            char ch = 0;
+            List initials = new ArrayList(27);
+            for (Term term : (Collection<Term>) terms) {
+                if (ch!=term.getInitial()) {
+                    ch = term.getInitial();
+                    initials.add(new Character(ch));
+                }
+            }
+            
             request.setAttribute("filter", filter);
             request.setAttribute("sort", sort);
             request.setAttribute("direction", direction);
             request.setAttribute("terms", terms);
+            request.setAttribute("initials", initials);
             
             if (sort==null || "".equals(sort) || "name".equalsIgnoreCase(sort)) {
                 map.put("html", WebContextFactory.get().forwardToString("/WEB-INF/jsp/glossary/gpTermsAlpha.jsp"));
