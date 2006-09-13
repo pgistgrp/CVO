16b411a2c729c3d8be1ba641d0aa52e401cd2fc0
kenny <>
2006-09-13 01:45:19 +0000

    initial.

---
+++ web/WEB-INF/src/org/pgist/cm/CoiceModelerAgent.java
@@ -0,0 +1,28 @@
+package org.pgist.cm;
+
+
+/**
+ * 
+ * @author kenny
+ *
+ */
+public class CoiceModelerAgent {
+    
+    
+    private ChoiceModelerService choiceModelerService;
+    
+    
+    public void setChoiceModelerService(ChoiceModelerService choiceModelerService) {
+        this.choiceModelerService = choiceModelerService;
+    }
+    
+    
+    /*
+     * ------------------------------------------------------------------------
+     */
+    
+    
+    
+    
+    
+}//class CoiceModelerAgent
