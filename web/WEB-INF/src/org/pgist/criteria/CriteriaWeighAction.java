b58dd61aa50d2a191b4a70d25180dfc62e14e615
kenny <>
2006-10-18 08:16:25 +0000

    initial.

---
+++ web/WEB-INF/src/org/pgist/criteria/CriteriaWeighAction.java
@@ -0,0 +1,57 @@
+package org.pgist.criteria;
+
+import java.util.Collection;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.pgist.discussion.InfoStructure;
+import org.pgist.discussion.SDService;
+
+
+/**
+ * 
+ * @author kenny
+ *
+ */
+public class CriteriaWeighAction extends Action {
+
+    
+    private CriteriaService criteriaService = null;
+    
+    private SDService sdService;
+    
+    
+    public void setCriteriaService(CriteriaService criteriaService) {
+        this.criteriaService = criteriaService;
+    }
+
+
+    public void setSdService(SDService sdService) {
+        this.sdService = sdService;
+    }
+
+
+    /*
+     * ------------------------------------------------------------------------
+     */
+    
+    
+    public ActionForward execute(
+            ActionMapping mapping,
+            ActionForm form,
+            javax.servlet.http.HttpServletRequest request,
+            javax.servlet.http.HttpServletResponse response
+    ) throws java.lang.Exception {
+        String isid = request.getParameter("isid");
+        
+        InfoStructure structure = sdService.getInfoStructureById(new Long(isid));
+        
+        request.setAttribute("structure", structure);
+        
+        return mapping.findForward("main");
+    }//execute()
+
+
+}//class CriteriaWeighAction
---
+++ web/WEB-INF/src/org/pgist/criteria/CriteriaWeight.java
@@ -0,0 +1,80 @@
+package org.pgist.criteria;
+
+import org.pgist.users.User;
+
+
+/**
+ * 
+ * @author kenny
+ *
+ * @hibernate.class table="pgist_criteria_weight" lazy="true"
+ */
+public class CriteriaWeight {
+    
+    
+    private Long id;
+    
+    private Criteria criteria;
+    
+    private User owner;
+    
+    private int weight = 0;
+    
+    
+    /**
+     * @hibernate.id generator-class="native"
+     */
+    public Long getId() {
+        return id;
+    }
+    
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    
+    /**
+     * @return
+     * 
+     * @hibernate.many-to-one lazy="true"
+     */
+    public Criteria getCriteria() {
+        return criteria;
+    }
+
+
+    public void setCriteria(Criteria criteria) {
+        this.criteria = criteria;
+    }
+
+
+    /**
+     * @return
+     * 
+     * @hibernate.many-to-one lazy="true"
+     */
+    public User getOwner() {
+        return owner;
+    }
+
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+
+    /**
+     * @hibernate.property not-null="true"
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+
+}//class CriteriaWeight
