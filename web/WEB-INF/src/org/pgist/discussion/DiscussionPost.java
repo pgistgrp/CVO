084eac828aae16d4f61166c4ec8fa383def92526
kenny <>
2006-10-19 20:16:36 +0000

    *** empty log message ***

--- web/WEB-INF/src/org/pgist/discussion/DiscussionPost.java
+++ web/WEB-INF/src/org/pgist/discussion/DiscussionPost.java
@@ -66,7 +66,7 @@ public class DiscussionPost extends GenericPost {
      * @hibernate.property not-null="true"
      */
     public int getViews() {
-        return views/2;
+        return views;
     }
 
 
