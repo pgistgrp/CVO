3014b27f369a6534ec9777c63bb869c2aeedcdb3
john <>
2006-11-13 21:53:26 +0000

    *** empty log message ***

--- web/WEB-INF/src/org/pgist/tags/FeedbackTag.java
+++ web/WEB-INF/src/org/pgist/tags/FeedbackTag.java
@@ -55,45 +55,24 @@ public class FeedbackTag extends SimpleTagSupport {
     public void doTag() throws JspException, IOException {
         JspWriter writer = getJspContext().getOut();
         
-        writer.write("<div id=\"feedbackForm\" style=\"display:none;\">");
-        writer.write("<div style='float:right;'><a href=\"javascript:Effect.toggle('feedbackForm','blind');\"><img src=\"/images/closeinactivesm.gif\" alt=\"close Feedback Form\" onmouseover=\"javascript:this.src='/images/closeactivesm.gif'; this.style.cursor='pointer'; \" onmouseout=\"javascript:this.src='/images/closeinactivesm.gif'; this.style.cursor='auto';\" border=\"0\"></a></div>");
-       
-        writer.write("<h3>Feedback/Bug Report Form</h3>");
-        writer.write("<p>If you came across a bug, please help us by reporting it to our development team. Please describe bugs and issues in as much detail as possible.</p>");
+        writer.write("<div id=\"feedbackForm\" class=\"box2\" style=\"display:none###;width:690px;padding:10px;text-align:left;\">");
+        writer.write("<div id=\"feedbackCloseBox\">");
+        writer.write("<a href=\"javascript:Effect.toggle('feedbackForm','blind');\"><img src=\"/images/closeinactivesm.gif\" alt=\"close this form\" onmouseover=\"javascript:this.src='/images/closeactivesm.gif'; this.style.cursor='pointer'; \" onmouseout=\"javascript:this.src='/images/closeinactivesm.gif'; this.style.cursor='auto';\" border=\"0\" class=\"floatRight\"></a></div>");
+        writer.write("<h3 class=\"headerColor clearBoth\">Feedback/Bug Report Form</h3>");
         
-        //      external js files
         writer.write("<script src=\"/dwr/interface/SystemAgent.js\"></script>");
+        writer.write("<script>var detect = '<p>***USER SYSTEM INFO*** </p>';detect += navigator.userAgent;function createFeedback() {  s = $('feedback_input').value;  if (s.length==0) { alert('Please input your feedback.'); return; }  s += detect;   action = 'cctView.do';  SystemAgent.createFeedback(    {feedback:s, action:action}, function(data) {      if (data.successful) { alert('Your feedback is submitted. Thank you.'); $('feedback_input').value=''; }      else { alert(data.reason); }    }  );}</script>");
+        writer.write("<div id=\"feedbackDiv\"><fieldset>");
         
-        writer.write("<script>");
-        writer.write("var detect = '<p>***USER SYSTEM INFO*** </p>';");
-        writer.write("detect += navigator.userAgent;");
-        writer.write("function createFeedback() {");
-        writer.write("  s = $('feedback_input').value;");
-        writer.write("  if (s.length==0) { alert('Please input your feedback.'); return; }");
-        writer.write("  s += detect; ");
-        writer.write("  action = '"+action+"';");
-        writer.write("  SystemAgent.createFeedback(");
-        writer.write("    {feedback:s, action:action}, function(data) {");
-        writer.write("      if (data.successful) { alert('You feedback is submitted. Thank you.'); $('feedback_input').value=''; }");
-        writer.write("      else { alert(data.reason); }");
-        writer.write("    }");
-        writer.write("  );");
-        writer.write("}");
-        writer.write("</script>");
+        writer.write("<textarea id=\"feedback_input\" style=\"width:100%;height:100px;border:1px solid #B5D77B;\" onclick=\"if(this.value==this.defaultValue){this.value = ''}\">If you came across a bug, please help us by reporting it to our development team. Please describe bugs and issues in as much detail as possible.</textarea><br>");
         
-        writer.write("<div id=\"feedbackDiv\"><fieldset>Feedback:<textarea id=\"feedback_input\"></textarea><br><input value=\"Submit\" onclick=\"createFeedback();new Effect.Fade('feedbackForm');\" type=\"button\"></div>");
-
-        writer.write("</fieldset></div>");
+        writer.write("<input value=\"Submit\" onclick=\"createFeedback();new Effect.Fade('feedbackForm');\" type=\"button\" class=\"floatRight\"></div></fieldset></div>");
         
-        //user msg
-        writer.write("<p>Found a bug? Problem accessing a part of the page? <a href=\"#feedbackForm\" onclick=\"javascript:Effect.toggle('feedbackForm','blind'); setTimeout('location.hash=\\\'#feedbackDiv\\\';',900);\">Send us feedback.</a></p>"); 
-        //Google Analytics
-        writer.write("<script src=\"http://www.google-analytics.com/urchin.js\" type=\"text/javascript\">");
-        writer.write("</script>");
-        writer.write("<script type=\"text/javascript\">");
-        writer.write("_uacct = \"UA-797433-1\";");
-        writer.write("urchinTracker();");
-        writer.write("</script>");
+        
+      
+        writer.write("<p>Found a bug? Problem accessing a part of the page? <a href=\"#feedbackForm\" onclick=\"javascript:Effect.toggle('feedbackForm','blind'); setTimeout('location.hash=\'#feedbackDiv\';',900);\">Send us feedback.</a></p>");
+        writer.write("<script src=\"http://www.google-analytics.com/urchin.js\" type=\"text/javascript\"></script>");
+        writer.write("<script type=\"text/javascript\">_uacct = \"UA-797433-1\";urchinTracker();</script>");
     }//doTag()
     
     
