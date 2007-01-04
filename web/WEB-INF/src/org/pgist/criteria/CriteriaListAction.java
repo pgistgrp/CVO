7e35e522e5b86e3dd8d37e6e07e76ead69faf869
john <>
2007-01-04 01:47:11 +0000

    *** empty log message ***

---
+++ web/WEB-INF/src/org/pgist/criteria/CriteriaListAction.java
@@ -0,0 +1,42 @@
+package org.pgist.criteria;
+
+import java.util.Collection;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.pgist.criteria.CriteriaService;
+import org.pgist.cvo.CCTForm;
+
+public class CriteriaListAction {
+	
+	  private CriteriaService criteriaService = null;
+	    
+	    
+	    public CriteriaListAction() {
+	    }
+	    
+	    
+	    public void setCriteriaService(CriteriaService criteriaService) {
+	        this.criteriaService = criteriaService;
+	    }
+
+	    
+	    public ActionForward execute(
+	            ActionMapping mapping,
+	            ActionForm form,
+	            javax.servlet.http.HttpServletRequest request,
+	            javax.servlet.http.HttpServletResponse response
+	    ) throws java.lang.Exception {
+
+	    	Collection ccts = criteriaService.getCCTs();
+	    	
+	    	request.setAttribute("ccts", ccts);
+	    	
+	        request.setAttribute("PGIST_SERVICE_SUCCESSFUL", true);
+	        
+	        return mapping.findForward("list");
+	    }//execute()
+	
+	
+} //CriteriaListAction();
