27f8774e2c4ac36f9f73583b512ad0852040c7c1
paulin <>
2007-03-22 20:34:31 +0000

    Updated the tag

--- web/WEB-INF/src/org/pgist/tags/PgistELFunctions.java
+++ web/WEB-INF/src/org/pgist/tags/PgistELFunctions.java
@@ -1,6 +1,11 @@
 package org.pgist.tags;
 
+import java.io.IOException;
 import java.util.Collection;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 
 /**
@@ -14,13 +19,39 @@ import java.util.Collection;
  * @author kenny
  *
  */
-public class PgistELFunctions {
+public class PgistELFunctions extends SimpleTagSupport {
     
+    private Collection collection;
+    private Object object;
     
-    public static boolean contains(Collection collection, Object object) {
+    public Collection getCollection() {
+		return collection;
+	}
+
+	public void setCollection(Collection collection) {
+		this.collection = collection;
+	}
+
+	public Object getObject() {
+		return object;
+	}
+
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+	public static boolean contains(Collection collection, Object object) {
         if (collection==null) return false;
         return collection.contains(object);
     }//contains()
     
-    
+    public void doTag() throws JspException, IOException {
+        JspWriter writer = getJspContext().getOut();
+        if(contains(this.collection, this.object)) {
+            writer.write("true");        	        	
+        } else {
+            writer.write("false");        	
+        }
+    	
+    }
 }//class PgistELFunctions
