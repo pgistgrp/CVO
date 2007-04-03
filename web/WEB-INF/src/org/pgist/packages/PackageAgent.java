3f6890f27c93ff135b1d622facf5d115a5e69e67
paulin <>
2007-04-03 01:54:53 +0000

    *** empty log message ***

--- web/WEB-INF/src/org/pgist/packages/PackageAgent.java
+++ web/WEB-INF/src/org/pgist/packages/PackageAgent.java
@@ -63,14 +63,16 @@ public class PackageAgent {
             Long pkgId = new Long((String) params.get("pkgId"));
             Long altId = new Long((String) params.get("altId"));
             boolean deleting = "true".equals((String) params.get("deleting"));            
-//            UserPackage userPkg;
-//            if(deleting) {
-//            	userPkg = this.packageService.removeProjectAlternative(pkgId, altId);
-//            } else {
-//            	userPkg = this.packageService.addProjectAlternative(pkgId, altId);
-//            }
-//                        
-//            request.setAttribute("package", userPkg);
+        	System.out.println("MATT: UsrPkg = " + pkgId + " altId = " + altId + " deleting = " + deleting);            
+            UserPackage userPkg;
+            if(deleting) {
+            	userPkg = this.packageService.removeProjectAlternative(pkgId, altId);
+            } else {
+            	userPkg = this.packageService.addProjectAlternative(pkgId, altId);
+            }
+            System.out.println("MATT: put in the string " + map.get("html"));
+                        
+            request.setAttribute("package", userPkg);
             map.put("html", WebContextFactory.get().forwardToString("/WEB-INF/jsp/packages/createPackage_summary.jsp"));            
             
             map.put("successful", true);
@@ -117,14 +119,14 @@ public class PackageAgent {
             Long altId = new Long((String) params.get("altId"));
             boolean deleting = "true".equals((String) params.get("deleting"));      
 System.out.println("MATT: UsrPkg = " + pkgId + " altId = " + altId + " deleting = " + deleting);            
-//            UserPackage userPkg;
-//            if(deleting) {
-//            	userPkg = this.packageService.removeFundingAlternative(pkgId, altId);
-//            } else {
-//            	userPkg = this.packageService.addFundingAlternative(pkgId, altId);
-//            }
-//                        
-//            request.setAttribute("package", userPkg);
+            UserPackage userPkg;
+            if(deleting) {
+            	userPkg = this.packageService.removeFundingAlternative(pkgId, altId);
+            } else {
+            	userPkg = this.packageService.addFundingAlternative(pkgId, altId);
+            }
+                        
+            request.setAttribute("package", userPkg);
             map.put("html", WebContextFactory.get().forwardToString("/WEB-INF/jsp/packages/createPackage_summary.jsp"));            
             System.out.println("MATT: put in the string " + map.get("html"));
             map.put("successful", true);
