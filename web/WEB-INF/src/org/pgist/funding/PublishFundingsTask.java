2627f5f004dc1076d22623f0b32033e46109ec9e
kenny <>
2007-05-02 02:51:31 +0000

    initial.

---
+++ web/WEB-INF/src/org/pgist/funding/PublishFundingsTask.java
@@ -0,0 +1,51 @@
+package org.pgist.funding;
+
+import org.pgist.discussion.InfoStructure;
+import org.pgist.wfengine.EnvironmentInOuts;
+import org.pgist.wfengine.WorkflowTask;
+
+
+/**
+ * Automatic workflow task for publishing fundings.
+ * 
+ * @author kenny
+ */
+public class PublishFundingsTask implements WorkflowTask {
+    
+    
+    public static final String IN_CCT_ID = "cct_id";
+    
+    public static final String IN_SUITE_ID = "suite_id";
+    
+    public static final String OUT_ISID = "is_id";
+    
+    
+    private FundingService fundingService;
+    
+    
+    public void setFundingService(FundingService fundingService) {
+        this.fundingService = fundingService;
+    }
+
+
+    /*
+     * ------------------------------------------------------------------------
+     */
+    
+    
+    public void execute(EnvironmentInOuts inouts) throws Exception {
+        System.out.println("@ PublishFundingsTask.execute()");
+        
+        Long suiteId = new Long(inouts.getIntValue(IN_SUITE_ID));
+        
+        Long cctId = new Long(inouts.getIntValue(IN_CCT_ID));
+        
+        System.out.printf("\n\n\n(%d, %d)\n\n\n", suiteId, cctId);
+        
+        InfoStructure structure = fundingService.publish(cctId, suiteId);
+        
+        inouts.setIntValue(OUT_ISID, structure.getId().intValue());
+    }//execute()
+    
+    
+}//class PublishFundingsTask
