166d36a18dc459f2506f752438ca245236ebe6f3
paulin <>
2007-05-03 22:40:38 +0000

    *** empty log message ***

--- web/WEB-INF/src/org/pgist/packages/PackageAction.java
+++ web/WEB-INF/src/org/pgist/packages/PackageAction.java
@@ -4,6 +4,8 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.pgist.funding.FundingService;
+import org.pgist.projects.ProjectService;
 
 
 /**
@@ -12,6 +14,10 @@ import org.apache.struts.action.ActionMapping;
  * The action accepts two parameters:
  * <ul>
  *   <li>pkgId - int, an id of a ClusteredPackage object</li>
+ *   <li>pkgSuiteId - the id of a specified PackageSuite object</li>
+ *   <li>projSuiteId - the id of a specified PackageSuite object</li>
+ *   <li>fundSuiteId - the id of a specified FundingSuite object</li>
+ *   <li>critSuiteId - the id of a specified CriteriaSuite object</li>
  * </ul>
  * 
  * The action will always forward to the jsp page specified in struts-config.xml
@@ -33,7 +39,18 @@ public class PackageAction extends Action {
         this.packageService = packageService;
     }
 
+    private ProjectService projectService;
+    
+    public void setProjectService(ProjectService projectService) {
+        this.projectService = projectService;
+    }
 
+    private FundingService fundingService;
+    
+    public void setFundingService(FundingService fundingService) {
+        this.fundingService = fundingService;
+    }
+    
     /*
      * ------------------------------------------------------------------------
      */
@@ -46,9 +63,24 @@ public class PackageAction extends Action {
             javax.servlet.http.HttpServletResponse response
     ) throws Exception {
     	String tempPkgId = request.getParameter("pkgId");
+    	String tempPackageSuiteId = request.getParameter("pkgSuiteId");
+    	String tempProjSuiteId = request.getParameter("projSuiteId");
+    	String tempFundSuiteId = request.getParameter("fundSuiteId");
+    	String tempCritSuiteId = request.getParameter("critSuiteId");
+    	
     	Long pkgId = new Long(tempPkgId);
+		Long packSuite = new Long(tempPackageSuiteId);
+		Long projSuite = new Long(tempProjSuiteId);
+		Long fundSuite = new Long(tempFundSuiteId);
+		Long critSuite = new Long(tempCritSuiteId);    	
+
 		ClusteredPackage uPack = this.packageService.getClusteredPackage(pkgId);    			
 		request.setAttribute("package", uPack);    		
+		request.setAttribute("projectRefs", projectService.getProjectSuite(projSuite).getReferences());
+		request.setAttribute("fundingRefs", fundingService.getFundingSuite(fundSuite).getReferences());
+		request.setAttribute("projSuiteId", projSuite);
+		request.setAttribute("fundSuiteId", fundSuite);
+		request.setAttribute("critSuiteId", critSuite);
     	
         request.setAttribute("PGIST_SERVICE_SUCCESSFUL", true);
         
