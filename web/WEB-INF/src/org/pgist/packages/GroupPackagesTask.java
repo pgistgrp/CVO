07e38f388e39253a803d2845bed1a7d5a70a59cb
kenny <>
2007-05-15 16:54:41 +0000

    initial.

---
+++ web/WEB-INF/src/org/pgist/packages/GroupPackagesTask.java
@@ -0,0 +1,42 @@
+package org.pgist.packages;
+
+import org.pgist.wfengine.EnvironmentInOuts;
+import org.pgist.wfengine.WorkflowTask;
+
+
+/**
+ * Automatic workflow task for grouping packages.
+ * 
+ * @author kenny
+ */
+public class GroupPackagesTask implements WorkflowTask {
+    
+    
+    public static final String IN_SUITE_ID = "suite_id";
+    
+    
+    private PackageService packageService;
+    
+    
+    public void setPackageService(PackageService packageService) {
+        this.packageService = packageService;
+    }
+
+
+    /*
+     * ------------------------------------------------------------------------
+     */
+    
+    
+    public void execute(EnvironmentInOuts inouts) throws Exception {
+        System.out.println("@ GroupPackagesTask.execute()");
+        
+        Long suiteId = new Long(inouts.getIntValue(IN_SUITE_ID));
+        
+        /*
+         * TODO: connect to Mike's clustering algorithm
+         */
+    }//execute()
+    
+    
+}//class GroupPackagesTask
