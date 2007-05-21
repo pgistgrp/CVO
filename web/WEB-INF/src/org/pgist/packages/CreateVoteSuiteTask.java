0dab7f212a5987c6915c7faff40e21e7fe33b90a
kenny <>
2007-05-21 23:21:48 +0000

    initial.

---
+++ web/WEB-INF/src/org/pgist/packages/CreateVoteSuiteTask.java
@@ -0,0 +1,44 @@
+package org.pgist.packages;
+
+import org.pgist.wfengine.EnvironmentInOuts;
+import org.pgist.wfengine.WorkflowTask;
+
+
+/**
+ * Automatic workflow task for creating vote suite.
+ * 
+ * @author kenny
+ */
+public class CreateVoteSuiteTask implements WorkflowTask {
+    
+    
+    public static final String IN_SUITE_ID = "pkg_suite_id";
+    
+    public static final String OUT_SUITE_ID = "suite_id";
+    
+    
+    private PackageService packageService;
+    
+    
+    public void setPackageService(PackageService packageService) {
+        this.packageService = packageService;
+    }
+
+
+    /*
+     * ------------------------------------------------------------------------
+     */
+    
+    
+    public void execute(EnvironmentInOuts inouts) throws Exception {
+        System.out.println("@ CreateVoteSuiteTask.execute()");
+        
+        Long pkgSuiteId = new Long(inouts.getIntValue(IN_SUITE_ID));
+        
+        PackageVoteSuite suite = packageService.createPackageVoteSuite(pkgSuiteId);
+        
+        inouts.setIntValue(OUT_SUITE_ID, suite.getId().intValue());
+    }//execute()
+    
+    
+}//class CreateVoteSuiteTask
