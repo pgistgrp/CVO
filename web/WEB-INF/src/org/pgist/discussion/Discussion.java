21d6dc0ed8c6711fa08a1ddb27ebbf1e61f5ffaa
john <>
2007-06-19 21:13:46 +0000

    *** empty log message ***

--- web/WEB-INF/src/org/pgist/discussion/Discussion.java
+++ web/WEB-INF/src/org/pgist/discussion/Discussion.java
@@ -54,7 +54,7 @@ public class Discussion implements Serializable {
     
     /**
      * @return
-     * @hibernate.property not-null="true"
+     * @hibernate.property not-null="false"
      */
     public Long getWorkflowId() {
         return workflowId;
