160c3d572d9853776733ba84f8c32db2794f023a
kenny <>
2007-07-18 23:25:13 +0000

    init.

---
+++ web/WEB-INF/src/org/pgist/tags/WorkflowInfoTag.java
@@ -0,0 +1,57 @@
+package org.pgist.tags;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+
+/**
+ * 
+ * @author kenny
+ *
+ */
+public class WorkflowInfoTag extends SimpleTagSupport {
+    
+    
+    private static final long serialVersionUID = 1L;
+    
+    
+    private JspFragment jspBody;
+
+
+    public void setJspBody(JspFragment jspBody) {
+        this.jspBody = jspBody;
+    }//setJspBody()
+    
+    
+    /*
+     * ------------------------------------------------------------------------
+     */
+    
+    
+    public void doTag() throws JspException, IOException {
+        PageContext context = (PageContext) getJspContext();
+        HttpServletRequest request = (HttpServletRequest) context.getRequest();
+        
+        String workflowId = request.getParameter("workflowId");
+        String contextId = request.getParameter("contextId");
+        String activityId = request.getParameter("activityId");
+        
+        JspWriter writer = getJspContext().getOut();
+        writer.write("{");
+        writer.write("workflowId:");
+        writer.write(workflowId);
+        writer.write(", contextId:");
+        writer.write(activityId);
+        writer.write("activityId:");
+        writer.write(workflowId);
+        writer.write("}");
+    }//doTag()
+    
+    
+}//class WorkflowInfoTag
