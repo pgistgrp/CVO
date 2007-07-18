543c8c3f58704cc40b939e4f239c84e5bb81e74f
kenny <>
2007-07-18 21:58:38 +0000

    init.

---
+++ web/WEB-INF/src/org/pgist/tags/URLTag.java
@@ -0,0 +1,78 @@
+package org.pgist.tags;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+
+/**
+ * 
+ * @author kenny
+ *
+ */
+public class URLTag extends SimpleTagSupport {
+    
+    
+    private static final long serialVersionUID = 1L;
+    
+    
+    private String page = "";
+    
+    private String params = "";
+    
+    private JspFragment jspBody;
+
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+
+    public void setParams(String params) {
+        this.params = params;
+    }
+
+
+    /*
+     * ------------------------------------------------------------------------
+     */
+    
+    
+    public void setJspBody(JspFragment jspBody) {
+        this.jspBody = jspBody;
+    }//setJspBody()
+    
+    
+    public void doTag() throws JspException, IOException {
+        PageContext context = (PageContext) getJspContext();
+        HttpServletRequest request = (HttpServletRequest) context.getRequest();
+        
+        String workflowId = request.getParameter("workflowId");
+        String contextId = request.getParameter("contextId");
+        String activityId = request.getParameter("activityId");
+        
+        JspWriter writer = getJspContext().getOut();
+        writer.write("<a href=\"");
+        writer.write(page);
+        writer.write("?");
+        writer.write("workflowId=");
+        writer.write(workflowId);
+        writer.write("&contextId=");
+        writer.write(contextId);
+        writer.write("&activityId=");
+        writer.write(activityId);
+        writer.write("&");
+        writer.write(params);
+        writer.write("\">");
+        jspBody.invoke(writer);
+        writer.write("</a>");
+    }//doTag()
+    
+    
+}//class URLTag
