750985ebf0d29086bfe9989eb1291e59321ddbc6
guirong <>
2007-08-08 21:55:11 +0000

    *** empty log message ***

--- initdata/final-init.sql
+++ initdata/final-init.sql
@@ -1,4 +1,7 @@
 INSERT INTO pgist_situation_template (id, name, path) VALUES (1, 'LIT template', '/WEB-INF/config/template_LIT.xml');
 
---alter table pgist_travel_marker alter point type geometry;
---alter table pgist_travel_trip alter route type geometry;
+ALTER TABLE pgist_travel_marker DROP COLUMN point;
+ALTER TABLE pgist_travel_marker ADD COLUMN point geometry;
+ALTER TABLE pgist_travel_trip DROP COLUMN route;
+alter table pgist_travel_trip ADD COLUMN route geometry;
+
