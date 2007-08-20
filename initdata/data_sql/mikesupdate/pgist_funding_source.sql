4da6d7aec2330c35d1e6bef2b8144d23b8299f11
guirong <>
2007-08-20 00:50:58 +0000

    *** empty log message ***

---
+++ initdata/data_sql/mikesupdate/pgist_funding_source.sql
@@ -0,0 +1,46 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Started on 2007-08-19 17:47:23
+
+SET client_encoding = 'SQL_ASCII';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- TOC entry 2571 (class 0 OID 19399)
+-- Dependencies: 2144
+-- Data for Name: pgist_funding_source; Type: TABLE DATA; Schema: public; Owner: pgist
+--
+
+COPY pgist_funding_source (id, name, "type", alternative_id) FROM stdin;
+18	zTest	8	\N
+8744	Employer excise tax	1	\N
+8756	Commercial parking tax	7	\N
+8765	Toll on Alaska Way Viaduct	8	\N
+8785	Toll on I-405, north of I-90	8	\N
+8801	Toll on I-405, south of I-90	8	\N
+8823	Toll on SR 520 bridge	8	\N
+8832	Toll on I-90	8	\N
+8841	Toll on SR 167	8	\N
+8735	Sales tax on gas (currently there is no sales tax on gas)	5	\N
+8696	Motor vehicle excise tax increase (rate now 0.3%)	3	\N
+8623	Vehicle license fee	2	\N
+8714	Gas tax increase (rate now 37.5 cents per gallon)	4	\N
+8596	General sales tax increase (rate now 8.9%)	1	\N
+89400	Toll on I-5, north of I-90	8	\N
+89415	Toll on I-5, south of I-90	8	\N
+\.
+
+
+-- Completed on 2007-08-19 17:47:24
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ initdata/data_sql/mikesupdate/pgist_projects.sql
@@ -0,0 +1,49 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Started on 2007-08-19 17:46:41
+
+SET client_encoding = 'SQL_ASCII';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- TOC entry 2572 (class 0 OID 19540)
+-- Dependencies: 2177
+-- Data for Name: pgist_projects; Type: TABLE DATA; Schema: public; Owner: pgist
+--
+
+COPY pgist_projects (id, name, description, transmode, corridor_id, inclusive, alternative_id) FROM stdin;
+37	I-405 Improvements	null	1	\N	f	\N
+91	Enhanced Express Bus Service	null	2	\N	f	\N
+43	SR 167 Improvements	\N	1	\N	f	\N
+51	New Highway Segments	\N	1	\N	f	\N
+55	Snohomish SR 9 Improvements	\N	1	\N	f	\N
+58	Snohomish I-5 Access Improvements	\N	1	\N	f	\N
+94	Sounder Improvements	\N	2	\N	f	\N
+99	Snohomish County Transit Improvements	\N	2	\N	f	\N
+27	Alaska Way Viaduct	null	1	\N	t	\N
+105	Other Seattle Area Improvements	null	1	\N	f	\N
+64	Other Snohomish Road Improvements	null	1	\N	f	\N
+79	Light Rail Extension to the North	null	2	\N	t	\N
+73	Light Rail Extension to the Eastside	null	2	\N	t	\N
+86	Light Rail Extension to the South	null	2	\N	t	\N
+32	SR 520 Bridge	null	1	\N	t	\N
+83	Enhanced Transit: Downtown to Capital Hill via First Hill	null	2	\N	t	\N
+72856	Light Rail Extension Studies		2	\N	f	\N
+72944	I-90 Improvements		1	\N	f	\N
+48	Other Pierce Road Improvements	null	1	\N	f	\N
+\.
+
+
+-- Completed on 2007-08-19 17:46:42
+
+--
+-- PostgreSQL database dump complete
+--
+
