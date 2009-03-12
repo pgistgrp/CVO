c1ef7bd6f79e560089cc9aaa3ec73b10d3bffe88
kenny <>
2009-03-12 05:49:24 +0000

    *** empty log message ***

--- web/WEB-INF/jython/Report.py
+++ web/WEB-INF/jython/Report.py
@@ -3,6 +3,9 @@
 #     report : org.pgist.sarp.report.Report
 #     output : a string, file path of the web pages for output
 
-print "report object", report.id
-print "output file", output
+print "================= report object", report.id
+print "================= output file", output
 
+myfile = open(output,"w")
+myfile.write("Content of the report!")
+myfile.close()
