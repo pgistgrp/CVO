9b85325448a0a605c21cc7c9efe2be21ac51606c
michalis <>
2009-08-20 04:13:47 +0000

    *** empty log message ***

---
+++ web/WEB-INF/src/org/pgist/exceptions/AssocExistsException.java
@@ -0,0 +1,14 @@
+package org.pgist.exceptions;
+
+public class AssocExistsException extends Exception {
+    
+    
+    private static final long serialVersionUID = 1L;
+    
+
+    public AssocExistsException(String message) {
+        super(message);
+    }
+    
+
+}
