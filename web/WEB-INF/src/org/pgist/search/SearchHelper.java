e05cd04789d96f1c8150889d42be7c2708c9666d
Mary Roderick <roderimj@uw.edu>
2015-07-23 14:53:19 -0400

    re-added missing method

--- web/WEB-INF/src/org/pgist/search/SearchHelper.java
+++ web/WEB-INF/src/org/pgist/search/SearchHelper.java
@@ -50,6 +50,11 @@ public class SearchHelper {
     }//getIndexSearcher()
 
 
+    public IndexSearcher getIndexSearcher(IndexReader reader) throws IOException {
+        return new IndexSearcher(reader);
+    }//getIndexSearcher()
+
+
     public IndexWriter getIndexWriter() throws IOException {
         return getIndexWriter(new StandardAnalyzer());
     }//getIndexWriter()
